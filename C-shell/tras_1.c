a408e88eb0b501989d7b5b662d923069975004da
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2020-10-25 23:30:12 +0200

    Create tras_1.c

---
+++ C-shell/tras_1.c
@@ -0,0 +1,32 @@
+include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+        int fd,fd1,broi;
+        char buff[15]="";
+        fd1=open("x1", O_CREAT|O_TRUNC|O_RDWR,0666);
+        write(fd1,"I am a student Computer science!!!",34);
+        lseek(fd1,0,0);
+        while((broi=read(fd1,buff,10))!=0)
+        {
+                write(1,buff,broi);
+                write(1,"ECHO",4);
+                printf("\n%d\n",broi);
+        }
+
+                printf("\n%d\n",broi);
+}
+~                                                                               
+~    
+
+//I am a stuECHO
+//10
+//dent CompuECHO
+//10
+//ter sciencECHO
+//10
+//e!!!ECHO
+//4
+
+//0
