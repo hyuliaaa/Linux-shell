ac3437139c6fa48e431516bc7e46af6ef178400d
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2020-11-02 21:03:35 +0200

    Create zad6.c

---
+++ C-shell/week4/zad6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+int main(int argc,char *argv[])
+{
+	for(int i=0; i<10;i++)
+	{
+		write(1,"EXAMPLE\n",8);
+	}
+	int fd=open(argv[1],O_CREAT| O_WRONLY, 0777);
+	if(fd==-1)
+	{
+		return 1;
+	}
+	int newfd=dup(1);
+	close(1);
+	int nfd=dup(fd);
+	close(fd);
+	
+	char str[50];
+	for(int i=0; i<10;i++)
+	{
+		sprintf(str,"i=%d\n",i);
+		write(nfd,str,strlen(str));
+	}
+	int fd3=dup(newfd);	
+	close(newfd);
+	
+	for(int i=0; i<10; i++)
+	{
+		write(fd3,"HELLO\n",6);
+	}
+	
+	return 0;
+}
