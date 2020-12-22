7e75dd9d588659d7c90c6eb07347e335c4ffc0aa
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2020-12-22 17:32:25 +0200

    Add files via upload

---
+++ C-shell/week2/zad2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+int main(int argc, char *argv[])
+{
+	int fd=open(argv[1], O_RDONLY);
+	if (fd==-1)
+	{
+		printf("Could not open the file");
+		return 1;
+	}
+	char buffer[16]="";
+	for(int i=5; i>=1; i--)
+	{
+		read(fd,buffer,i);
+		write(1,buffer,i);
+		printf("\n");
+	}
+	return 0;
+}
