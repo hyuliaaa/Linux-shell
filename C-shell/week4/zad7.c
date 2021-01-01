dbcec55f1f0fec96aac48fa3f58e0a8c3a175dba
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-01-01 21:25:43 +0200

    Update zad7.c

--- C-shell/week4/zad7.c
+++ C-shell/week4/zad7.c
@@ -29,3 +29,37 @@ return 0;
 }
 
 
+
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+int main(int argc, char *argv[])
+{
+	int fd1=open(argv[1], O_RDONLY);
+	if(fd1==-1)
+	{
+		printf("cannot be opened");
+		return 1;
+	}
+	int fd2=open(argv[2],O_WRONLY| O_CREAT, 0777);
+	
+	if(fd2==-1)
+	{
+		printf("cannot be opened");
+		return 1;
+	}
+	close(0);
+	dup(fd1);
+	close(1);
+	dup(fd2);
+	char buff[1];
+	while(read(0,buff,1))
+	{
+		write(1,buff,1);
+	}
+
+return 0;
+}
