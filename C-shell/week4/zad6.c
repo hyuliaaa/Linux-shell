c46dd2f8e699c5db05b1b30f453a36d4144654eb
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-01-01 21:11:21 +0200

    Update zad6.c

--- C-shell/week4/zad6.c
+++ C-shell/week4/zad6.c
@@ -36,3 +36,49 @@ int main(int argc,char *argv[])
 	
 	return 0;
 }
+
+
+
+
+
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+	for(int i=0; i<10; i++)
+	{
+		write(1,"EXAMPLE", 7);
+		write(1,"\n",1);
+	}
+	int fd=open(argv[1], O_CREAT| O_WRONLY,0777);
+	if(fd==-1)
+	{
+		printf("file cannot be opened");
+		return 1;
+	}
+	char str[50];	
+	int nwf=dup(1);
+	close(1);
+	int nfd=dup(fd);
+	close(fd);
+	for(int i=0; i<10; i++)
+	{
+		
+		sprintf(str,"i=%d\n",i);
+		write(1,str,strlen(str));
+	}
+	close(1);
+	dup(nwf);
+	close(nwf);
+	
+	for(int i=0; i<10; i++)
+	{
+		write(1,"HELLO", 5);
+		write(1,"\n",1);
+	}
+return 0;
+}
