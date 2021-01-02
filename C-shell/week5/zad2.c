636fcfa8a692c7685b9301dc2e9a69b903efcb23
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-01-02 15:27:16 +0200

    Update zad2.c

--- C-shell/week5/zad2.c
+++ C-shell/week5/zad2.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 
-	execlp("grp","grp", "int", argv[1],NULL);
+	execlp("grp","grp", "int", argv[1],NULL); //след като командата е грешна се преминава към втория ред, иначе не се
 	printf("Never printed");
 	return 0;
 }
