056d2425c0d8c1a82f649529082dcafad9a8d029
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-01-03 17:05:21 +0200

    Create zad7.c

---
+++ C-shell/week6/zad7.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int status=0;
+	int fd=open(argv[1], O_CREAT|O_TRUNC| O_WRONLY,0777);
+
+		int fd2=open(argv[2], O_CREAT| O_WRONLY,0777);
+	if (fd==-1)
+	{
+		printf("Cannot be opened");
+		return 1;
+	}
+	int pid=fork();
+	if(pid==-1)
+	{
+		perror("Cannot fork");
+		return 1;
+	}	
+	else if (pid > 0)
+	{
+		wait(&status);
+		write(fd,"hello1",6);
+	}
+	else
+	{
+	int nf=	dup(1);
+		close(1);
+		dup(fd2);
+		write(1,"hello2\n",6);
+		close(1);
+		dup(nf);
+		exit(status); //prektatyava procesa nezabavno
+
+	}
+	write(1,"hello3\n",6);
+	write(fd2, "hello4\n",6);
+	
+return 0;
+}
+
+
+//output: hello3
+//f1:hello1
+//f2:hello2hello4
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int status=0;
+	int fd=open(argv[1], O_CREAT|O_TRUNC| O_WRONLY,0777);
+
+		int fd2=open(argv[2], O_CREAT| O_WRONLY,0777);
+	if (fd==-1)
+	{
+		printf("Cannot be opened");
+		return 1;
+	}
+	int pid=fork();
+	if(pid==-1)
+	{
+		perror("Cannot fork");
+		return 1;
+	}	
+	else if (pid > 0)
+	{
+		wait(&status);
+		write(fd,"hello1",6);
+		exit(status);
+	}
+	else
+	{
+	int nf=	dup(1);
+		close(1);
+		dup(fd2);
+		write(1,"hello2\n",6);
+		close(1);
+		dup(nf);
+		exit(status);
+
+	}
+	write(1,"hello3\n",6);
+	write(fd2, "hello4\n",6);
+	
+return 0;
+}
+//no output because of exit
+//f1:hello1
+//f2:hello2
