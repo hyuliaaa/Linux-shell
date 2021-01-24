718821f1b98d06f2779b5f9ef91090a7817dd0bd
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-01-24 20:15:13 +0200

    Create 2014tras.c

---
+++ C-shell/examPreparation/2014tras.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+int main(int argc, char *argv[])
+{
+	int pid,fd;
+	char buf[5];
+	if((pid=fork())==-1)
+	{
+		write(2, "Fork failed\n",13);
+		return 1;
+	}
+	if(pid!=0)
+	{
+		int status=0;
+		wait(&status);	
+		fd=open("asdf.txt", O_RDWR | O_APPEND);
+		write(fd,"7",1);
+		lseek(fd,0,SEEK_SET);
+		int n=read(fd,buf,5);
+		write(1,buf,n);
+	}
+	else
+	{
+		fd=open("asdf.txt", O_CREAT | O_WRONLY, 0644);
+		dup2(fd,1);
+		close(fd);
+		execlp("echo","echo","123456",NULL);
+	}
+	return 0;
+}
